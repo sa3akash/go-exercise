9337e83474c85d5e3fb31c76dde09051a97d81a6
Shakil Ahmed <sa3akash@gmail.com>
2025-11-14 21:09:51 +0600

    add simple value printing examples

---
+++ 2-simple-value/main.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func main()  {
+	fmt.Println(1)
+	fmt.Println(1+1)
+	fmt.Println("Hello, " + "World!")
+	fmt.Println(true)
+	fmt.Println(false)
+	fmt.Println(6.265)
+
+}
